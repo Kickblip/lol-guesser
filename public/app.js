d165dba3c488ca123a15e843254111fc9663b643
Kickblip <wyatt.zilker@gmail.com>
2024-01-25 15:40:01 -0700

    comments

--- public/app.js
+++ public/app.js
@@ -5,6 +5,8 @@ let sample_size = starting_size
 let currentBlob
 let currentImageString
 
+// takes a blob (Binary Large OBject), pixelates it, and updates the HTML file with the new image
+// the image blobs come from the server
 function drawImageFromBlob(blob) {
     return new Promise((resolve, reject) => {
         let reader = new FileReader()
@@ -46,31 +48,37 @@ function drawImageFromBlob(blob) {
         .catch((error) => console.error("Error:", error))
 }
 
+// requests an image blob and answer string from the server
 function loadImage() {
     fetch("/random-image")
         .then((response) => {
-            currentImageString = response.headers.get("X-Image-String") // save the current image string
+            currentImageString = response.headers.get("X-Image-String") // save the answer string from the headers
             return response.blob()
         })
         .then((blob) => {
-            currentBlob = blob // save the current blob
-            return drawImageFromBlob(blob)
+            currentBlob = blob // save the image blob
+            return drawImageFromBlob(blob) // draw the image on the HTML using the blob
         })
 }
 
 window.onload = loadImage
 
+// add a listener to the guess-form input box that is fired when they submit it
 document.getElementById("guess-form").addEventListener("submit", function (event) {
     event.preventDefault()
+
+    // get the text from the HTML input box
     const guess = document.getElementById("guess-input").value.toLowerCase()
 
     // strip the guess of spaces, numbers, and special characters
     const strippedGuess = guess.replace(/[^a-z]/g, "")
 
+    // check the users guess against the correct answer
     if (strippedGuess === currentImageString) {
         var img = document.getElementById("display-image")
         var rect = img.getBoundingClientRect()
 
+        // confetti animation from library (not important)
         confetti({
             particleCount: 200,
             spread: 100,
@@ -90,14 +98,15 @@ document.getElementById("guess-form").addEventListener("submit", function (event
     } else {
         const image = document.getElementById("display-image")
 
+        // CSS animation jiggles the image when they are wrong
         image.classList.add("jiggle")
 
         setTimeout(function () {
             image.classList.remove("jiggle")
-        }, 1000)
+        }, 1000) // remove the jiggle class after some time to make it stop
 
-        document.getElementById("guess-input").value = ""
+        document.getElementById("guess-input").value = "" // clear the guess input field
         sample_size = Math.max(1, sample_size - reduce_amount)
-        drawImageFromBlob(currentBlob) // redraw the current image
+        drawImageFromBlob(currentBlob) // redraw the current image with a smaller sample size (less pixelated)
     }
 })
--- server/server.js
+++ server/server.js
@@ -8,8 +8,12 @@ const port = process.env.PORT || 3000
 // load the JSON file containing the answers
 const imageStrings = require("./champs.json")
 
+// tells express where to find the static files
+// also sends index.html when the root ('/') is requested
+// the root is requested automatically so index.html and its attached css and js files are sent automatically
 app.use(express.static(path.join(__dirname, "..", "public")))
 
+// api endpoint to pick a random image and send it to the client
 app.get("/random-image", (req, res) => {
     fs.readdir(path.join(__dirname, "..", "public", "images"), (err, files) => {
         if (err) {
@@ -19,16 +23,18 @@ app.get("/random-image", (req, res) => {
         // filter the files to exclude '.DS_Store'
         const imageFiles = files.filter((file) => file !== ".DS_Store")
 
+        // pick a random json entry (contains image file name and answer)
         const randomIndex = Math.floor(Math.random() * imageFiles.length)
         const randomImage = imageFiles[randomIndex]
 
-        // get the unique string for the random image
+        // this is the string the user will have to guess (e.g. 'aatrox')
         const randomImageString = imageStrings[randomImage]
 
-        // send the string in the headers of the response
+        // send the string (answer) in the headers of the response so client can check if the guess is correct
         res.setHeader("X-Image-String", randomImageString)
         res.sendFile(path.join(__dirname, "..", "public", "images", randomImage))
     })
 })
 
+// start the server on port 3000
 app.listen(port, () => console.log(`Listening on port ${port}`))
